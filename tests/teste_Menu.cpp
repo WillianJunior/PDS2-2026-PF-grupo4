c847a23a3241ccecac727ec58147676803f9e115
Isaque Daniel Freire Martins <isaquemochilapreta@ufmg.br>
2026-05-21 11:01:08 -0300

    Adicao de doctest classe MENU

--- tests/teste_Menu.cpp
+++ tests/teste_Menu.cpp
@@ -2,3 +2,19 @@
 #include "Menu.hpp"
 
 // Os TEST_CASEs serão escritos aqui
+TEST_CASE("TESTE DE CONSTRUCAO DO MENU") {
+    Menu menuTeste;
+    CHECK(true); 
+}
+
+TEST_CASE("TESTE DE MENU FUNCAO iniciarJogo") {
+    Menu menuTeste;
+    menuTeste.iniciarJogo();
+    // NÃO POSSUI TESTE, FUNÇÃO VOID QUE IMPRIME E EXECUTA AÇÕES SEM RETORNO
+}
+
+TEST_CASE("TESTE DE MENU FUNCAO escolherPersonagemInicial") {
+    Menu menuTeste;
+    menuTeste.escolherPersonagemInicial();
+    // NÃO POSSUI TESTE, FUNÇÃO VOID QUE IMPRIME E EXECUTA AÇÕES SEM RETORNO
+}
