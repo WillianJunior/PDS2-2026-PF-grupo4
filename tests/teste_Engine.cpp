e6fdb183c625681af0f53f76970565e2f8a2dca8
M-Jota <mayconjonnatha2000@gmail.com>
2026-06-25 23:24:12 -0300

    atualizando teste para nova engine

--- tests/teste_Engine.cpp
+++ tests/teste_Engine.cpp
@@ -1,14 +1,21 @@
 #include "doctest.h"
 #include "Engine.hpp"
+#include "Utils.hpp"
 #include <sstream>
 #include <iostream>
 
 TEST_CASE("ENGINE - Teste 01: Verifica encerramento via Menu") {
+    // Ativa as flags estáticas para garantir comportamento determinístico e sem delays
+    Engine::modoTeste = true; 
+    Utils::modoTeste = true;
+
     Engine jogo;
 
+    // entrada do usuario
     std::stringstream simulacaoEntrada("2\n");
     std::streambuf* cinAntigo = std::cin.rdbuf(simulacaoEntrada.rdbuf());
 
+    // redirecionamento do buffer
     std::stringstream bufferSaida;
     std::streambuf* coutAntigo = std::cout.rdbuf(bufferSaida.rdbuf());
 
@@ -21,5 +28,6 @@ TEST_CASE("ENGINE - Teste 01: Verifica encerramento via Menu") {
     
     CHECK(saida.find("1 - New Game") != std::string::npos);
     CHECK(saida.find("2 - Out") != std::string::npos);
-    CHECK(saida.find("Inicialização abortada. Encerrando o sistema.") != std::string::npos);
+    
+    CHECK(saida.find("Encerrando o sistema. Até a próxima compilação!") != std::string::npos);
 }
